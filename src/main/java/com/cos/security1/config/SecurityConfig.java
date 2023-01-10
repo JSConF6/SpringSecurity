b2f642a8d90a58bbd2df30c1bc5b70f5c299699d
JSConF <arare30030@naver.com>
2023-01-10 18:29:57 +0900

    Initial commit

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 된다.
+public class SecurityConfig{
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
+    @Bean
+    public BCryptPasswordEncoder encodePwd(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").hasRole("ROLE_ADMIN or ROLE_MANAGER")
+                .antMatchers("/admin/**").hasRole("ROLE_ADMIN")
+                .anyRequest().permitAll()
+        .and()
+        .formLogin()
+        .loginPage("/loginForm");
+
+        return http.build();
+    }
+}
